b1a1e6a08dc4bb0360aab980b621bbac3a436b4d
flavioaugustto <flavioaugustosilv@gmail.com>
2026-03-24 11:46:44 -0300

    Codigo pronto

--- src/main/java/com/example/tarefa/controllers/TarefaController.java
+++ src/main/java/com/example/tarefa/controllers/TarefaController.java
@@ -1,4 +1,36 @@
 package com.example.tarefa.controllers;
 
+import com.example.tarefa.models.TarefaModel;
+import com.example.tarefa.services.TarefaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/tarefas")
 public class TarefaController {
+
+    @Autowired
+    private TarefaService service;
+
+    @PostMapping
+    public TarefaModel salvar(@RequestBody TarefaModel tarefa) {
+        return service.salvar(tarefa);
+    }
+
+    @GetMapping
+    public List<TarefaModel> listar() {
+        return service.listar();
+    }
+
+    @GetMapping("/{id}")
+    public TarefaModel buscar(@PathVariable Long id) {
+        return service.buscar(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletar(@PathVariable Long id) {
+        service.deletar(id);
+    }
 }
--- src/main/java/com/example/tarefa/services/TarefaService.java
+++ src/main/java/com/example/tarefa/services/TarefaService.java
@@ -1,4 +1,31 @@
 package com.example.tarefa.services;
 
+import com.example.tarefa.models.TarefaModel;
+import com.example.tarefa.repositories.TarefaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class TarefaService {
+
+    @Autowired
+    private TarefaRepository repository;
+
+    public TarefaModel salvar(TarefaModel tarefa) {
+        return repository.save(tarefa);
+    }
+
+    public List<TarefaModel> listar() {
+        return repository.findAll();
+    }
+
+    public TarefaModel buscar(Long id) {
+        return repository.findById(id).orElse(null);
+    }
+
+    public void deletar(Long id) {
+        repository.deleteById(id);
+    }
 }
