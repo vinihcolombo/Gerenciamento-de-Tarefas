043afd00a4d074f08ad9cde781e1c27c5ff7bd3c
flavioaugustto <flavioaugustosilv@gmail.com>
2026-03-24 11:19:02 -0300

    Repositorio pronto

--- src/main/java/com/example/tarefa/repositories/TarefaRepository.java
+++ src/main/java/com/example/tarefa/repositories/TarefaRepository.java
@@ -1,4 +1,7 @@
 package com.example.tarefa.repositories;
 
-public interface TarefaRepository {
+import com.example.tarefa.models.TarefaModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TarefaRepository extends JpaRepository<TarefaModel, Long> {
 }
